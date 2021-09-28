dfde67bb307f9d19a13e4d1091b84b23b2f5c48e
gleissonbh <71742662+gleissonbh@users.noreply.github.com>
2021-09-28 13:53:28 -0300

    Initial commit

---
+++ exercicos/ex001/dh.js
@@ -0,0 +1,6 @@
+var saldo = 225.40
+if (saldo > 0) {
+    console.log ("Seu saldo está positivo! Gostaria de fazer um investimento?.")
+}if (saldo < 0){ 
+    console.log ("Seu saldo está negativo! Gostaria de fazer um empréstimo?.")
+}
